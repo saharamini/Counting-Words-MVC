169bd34901a08f592fb2c39c20af82ac1e2c9d28
Sahar <saharamini@users.noreply.github.com>
2017-08-14 00:31:30 -0400

    readme.md

---
+++ source_code/WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        // Get: Home/Index
+        public ActionResult Index(string id)
+        {
+            try
+            {
+                var result = new Countingcs().Countwords(id);
+               
+                Response.Write(result);
+                Response.End();
+                return View();
+
+            }
+            catch (Exception ex)
+            {
+                return View();
+            }
+
+            
+        }
+
+        // in RouteConfig we have id
+        public ActionResult About(string id)
+        {
+            ViewBag.Message = "Your application description page.";
+
+            // return View();
+            return Content("name="+ id);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ source_code/WebApplication/Models/Countingcs.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models
+{
+    public class Countingcs
+    {
+        public Hashtable table = new Hashtable();
+
+        public string Countwords(string value)
+        {
+            string result = String.Empty;
+            char[] delims = { ',', ' ', '.', '?', ';', '!', '"', '\'' };
+            string[] words = value.Trim().Split(delims, StringSplitOptions.RemoveEmptyEntries);
+
+            // add words to Hashtable
+            foreach (string next in words)
+            {
+                if (table[next] == null)
+                    table[next] = 1;
+                else
+                {
+                    int n = (int)table[next];
+                    table[next] = n + 1;
+                }
+
+            }
+
+            foreach (DictionaryEntry e in table)
+            {
+                result += e.Key.ToString();
+                if (Convert.ToInt32(e.Value) > 1)
+                {
+                    result += " ( " + e.Value.ToString() + " ) <br/> ";
+                }
+                else
+                {
+                    result += "<br/>";
+                }
+            }
+
+            return result;
+
+        }
+
+    }
+}
